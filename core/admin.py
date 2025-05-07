e111baf4a79670907b10386b7816a3c9d9531e0e
luansilva25 <luansilva250807@gmail.com>
2025-05-07 14:06:01 -0300

    FEAT: create admin painel #10

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from core.models import Enterprise, Sector, Telephone, Function, Position
 
-# Register your models here.
+admin.site.register(Enterprise)
+admin.site.register(Sector)
+admin.site.register(Telephone)
+admin.site.register(Function)
+admin.site.register(Position)
+
