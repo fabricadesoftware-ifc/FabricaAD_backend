bc015ae07bd77ece5c571706ec61523987873ddd
luansilva25 <luansilva250807@gmail.com>
2025-05-08 10:53:30 -0300

    FEAT: create position serializer #18

---
+++ core/serializers/position.py
@@ -0,0 +1,7 @@
+from core.models import Position
+from rest_framework.serializers import ModelSerializer
+
+class PositionSerializer(ModelSerializer):
+    class Meta:
+        model = Position
+        fields = '__all__'
