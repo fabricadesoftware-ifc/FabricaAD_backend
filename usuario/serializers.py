3382d149e884305baf16d601efd97cbb99aa0b17
luansilva25 <luansilva250807@gmail.com>
2025-05-10 11:47:20 -0300

    FEAT: adding hashers to users #48

--- usuario/serializers.py
+++ usuario/serializers.py
@@ -1,5 +1,5 @@
-from rest_framework.serializers import ModelSerializer, SlugRelatedField
-
+from rest_framework.serializers import ModelSerializer
+from django.contrib.auth.hashers import make_password
 from .models import Usuario
 
 
@@ -7,3 +7,11 @@ class UsuarioSerializer(ModelSerializer):
     class Meta:
         model = Usuario
         fields = "__all__"
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return validated_data
+    
+    def update(self, instance, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return validated_data
