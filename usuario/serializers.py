7a3a523b95c92bc594cdee6278cf70b027698b4e
luansilva25 <luansilva250807@gmail.com>
2025-05-10 13:01:56 -0300

    FEAT: associating employer creation with the telephone #50

--- usuario/serializers.py
+++ usuario/serializers.py
@@ -1,16 +1,33 @@
 from rest_framework.serializers import ModelSerializer
 from django.contrib.auth.hashers import make_password
 from .models import Usuario
+from core.models.telephone import Telephone
+from core.serializers.telephone import TelephoneSerializer
 
 
 class UsuarioSerializer(ModelSerializer):
+    telephones = TelephoneSerializer(many=True, required=False)
+    
     class Meta:
         model = Usuario
         fields = "__all__"
 
     def create(self, validated_data):
+        print(validated_data)
         validated_data['password'] = make_password(validated_data['password'])
-        return validated_data
+        telephones = validated_data.pop("telephones", [])
+        print(telephones)
+
+        user = super().create(validated_data)
+
+        if len(telephones) > 0:
+            for telephone in telephones:
+                print(telephone, telephones)
+                Telephone.objects.create(user_phone=user, **telephone)
+
+        return user
+
+
     
     def update(self, instance, validated_data):
         validated_data['password'] = make_password(validated_data['password'])
