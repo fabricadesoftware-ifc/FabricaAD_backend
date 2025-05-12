2392e76360001792d00a933c9b20c4871cc3e5d0
PedroHenmalaquias <pedromalaquias2223@gmail.com>
2025-05-12 16:10:59 -0300

    FEAT: topic model done [REFS: #53]

---
+++ core/models/topic.py
@@ -0,0 +1,9 @@
+from django.db import models
+from .enterprise import Enterprise
+
+class Topic(models.Model):
+    title = models.CharField(max_length=45, blank=False, null=False)
+    enterprise_topic = models.ForeignKey(Enterprise, on_delete=models.PROTECT, null=True, blank=True, default=None)
+
+    def __str__(self):
+        return self.title
