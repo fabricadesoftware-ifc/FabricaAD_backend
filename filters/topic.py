b1dc59c69723de1c37b9fd850368ad413c695564
Camila Ferreira de Almeida <mila.e.ferreira28@gmail.com>
2025-05-15 22:36:59 -0300

    FEAT: revert change in topic filter [refs #77]

--- filters/topic.py
+++ filters/topic.py
@@ -2,7 +2,7 @@ import django_filters
 from core.models import Topic
 
 class TopicFilter(django_filters.FilterSet):
-    topic_filter = django_filters.CharFilter(field_name="title", filelookup_expr="icontains")
+    topic_filter = django_filters.CharFilter(lookup_expr="icontains")
     
     class Meta:
         model = Topic
